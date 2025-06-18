8b6935772a8daf4a2393ce8cf790d71d1fe7b78d
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-06-18 19:07:53 -0300

    Criando visualização de employee

---
+++ src/utils/categories.ts
@@ -0,0 +1,30 @@
+import foodSvg from '../assets/food.svg'
+import othersSvg from '../assets/others.svg'
+import servicesSvg from '../assets/accommodation.svg'
+import transportSvg from '../assets/transport.svg'
+import accomodationSvg from '../assets/accommodation.svg'
+
+export const CATEGORIES = {
+    food: {
+        name: 'Alimentação',
+        icon: foodSvg
+    },
+    others: {
+        name: 'Outros',
+        icon: othersSvg
+    },
+    services: {
+        name: 'Serviços',
+        icon: servicesSvg
+    },
+    transport: {
+        name: 'Transporte',
+        icon: transportSvg
+    },
+    accomodation: {
+        name: 'Hospedagem',
+        icon: accomodationSvg
+    },
+}
+
+export const CATEGORIES_KEYS = Object.keys(CATEGORIES) as Array<keyof typeof CATEGORIES>
