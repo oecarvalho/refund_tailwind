9da823faf08f209a7ccf90af2b4fb7420003f725
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-06-19 14:43:21 -0300

    finalizando página de upload

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,18 +1,29 @@
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
-import { useState } from "react"
+import { use, useState } from "react"
+import { Upload } from "../components/Upload"
+import { Button } from "../components/Button"
 
 export function Refund(){
     const [category, setCategory] = useState('')
+    const [name, setName] = useState('')
+    const [amount, setAmount] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+    const [filename, setFilename] = useState<File | null>(null)
+
+    function onSubmit(e:React.FormEvent){
+        e.preventDefault()
+    }
+
     return (
-        <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+        <form onSubmit={onSubmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
             <header>
                 <h1 className="text-xl font-bold text-gray-100">Solicitação de Reembolso</h1>
                 <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso</p>
             </header>
 
-            <Input required legend="Título da Solicitação"/>
+            <Input required legend="Título da Solicitação" value={name} onChange={(e)=> setName(e.target.value)}/>
 
             <div className="flex gap-4">
                 <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
@@ -25,9 +36,14 @@ export function Refund(){
                     }
                 </Select>
 
-                <Input legend="Valor" required />
+                <Input legend="Valor" required value={amount} onChange={(e)=> setAmount(e.target.value)} />
             </div>
 
+
+            <Upload filename={filename && filename.name} onChange={(e)=> e.target.files && setFilename(e.target.files[0])}/>
+            <Button type="submit" isLoading={isLoading}>
+                Enviar
+            </Button>
         </form>
     )
 }
