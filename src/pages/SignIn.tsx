23ba53d7857e9cd3de2d3485070a189894d5c90b
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-07-12 20:41:22 -0300

    subindo ultimas atualizações

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,23 +1,49 @@
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
-import { useState } from "react";
+import { useActionState } from "react";
+import {z, ZodError} from 'zod'
+import { api } from "../services/api";
+import { AxiosError } from "axios";
 
+const signInScheme = z.object({
+    email: z.string().email({message: "informe o nome"}),
+    password: z.string().trim().min(1, {message: 'E-mail inválido!'}),
+})
 
 export function SignIn(){
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
+    const [state, formAction, isLoading] = useActionState(signIn, null)
 
-    function onSubmit(e: React.FormEvent){
-        e.preventDefault();
-        console.log(email, password)
+    async function signIn(_: any, formData: FormData){
+        try {
+            const data = signInScheme.parse({
+            email: formData.get('email'),
+            password: formData.get('password')
+        })
+
+        const response = await api.post('/sessions', data)
+        console.log(response.data)
+            
+        } catch (error) {
+            
+            if(error instanceof ZodError){
+                return {message: error.issues[0].message}
+            }
+
+            if (error instanceof AxiosError){
+                return {message: error.response?.data.message}
+            }
+
+            return {message: 'Não foi possível entrar!'}
+        }
+
     }
 
     return(
-        <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
-            <Input required legend="E-mail" type="mail" placeholder="Digite aqui o seu e-mail" onChange={(e)=>setEmail(e.target.value)}/>
-            <Input required legend="Senha" type="password" placeholder="Digite sua senha" onChange={(e)=>setPassword(e.target.value)}/>
-            <Button type="submit">Entrar</Button>
+        <form className="w-full flex flex-col gap-4" action={formAction}>
+            <Input name="email" required legend="E-mail" type="mail" placeholder="Digite aqui o seu e-mail" />
+            <Input name="password" required legend="Senha" type="password" placeholder="Digite sua senha" />
+            <p className="text-sm text-red-600 text-center my-4 font-medium">{state?.message}</p>
+            <Button type="submit" isLoading={isLoading}>Entrar</Button>
 
             <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center 
             hover:text-green-800 transition ease-linear">Criar Conta</a>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,6 +2,22 @@ import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 import { useState } from "react";
 
+import { api } from "../services/api";
+import { AxiosError } from "axios";
+import {z, ZodError} from 'zod'
+import { useNavigate } from "react-router";
+
+const signUpSchema = z.object({
+    name: z.string().trim().min(1, {message: "informe o nome"}),
+    email: z.string().email({message: 'E-mail inválido!'}),
+    password: z.string().min(6, {message: 'Senha deve ter pelo menos 6 digitos'}),
+
+    passwordConfirm: z.string({message: 'Confirme a senha'})
+}).refine((data)=> data.password === data.passwordConfirm, {
+    message: 'As senhas não são iguais',
+    path:['passwordConfirm']
+})
+
 
 export function SignUp(){
     const [name, setName] = useState('')
@@ -10,15 +26,46 @@ export function SignUp(){
     const [passwordConfirm, setPasswordConfirm] = useState('')
     const [isLoading, setIsLoading] = useState(false)
 
-    function onSubmit(e: React.FormEvent){
+    const navigate = useNavigate()
+
+   async function onSubmit(e: React.FormEvent){
         e.preventDefault();
-        console.log(name, email, password, passwordConfirm)
+        
+        try{
+            setIsLoading(true)
+
+            const data = signUpSchema.parse({
+                name,
+                email,
+                password,
+                passwordConfirm
+            })
+
+            await api.post('/users', data)
+
+            if(confirm('Cadastro Realizado. Ir para a tela de entrar?')){
+                navigate('/')
+            }
+
+        } catch(error){
+            if(error instanceof ZodError){
+                return alert(error.issues[0].message)
+            }
+
+            if(error instanceof AxiosError){
+                return alert(error.response?.data.message)
+            }
+
+            alert('Não foi possivel cadastrar')
+        } finally{
+            setIsLoading(false)
+        }
     }
 
     return(
         <form className="w-full flex flex-col gap-4" onSubmit={onSubmit}>
-            <Input required legend="E-mail" type="mail" placeholder="Digite aqui o seu e-mail" onChange={(e)=>setEmail(e.target.value)}/>
             <Input required legend="Nome" placeholder="Digite aqui o seu nome" onChange={(e)=>setName(e.target.value)}/>
+            <Input required legend="E-mail" type="mail" placeholder="Digite aqui o seu e-mail" onChange={(e)=>setEmail(e.target.value)}/>
             <Input required legend="Senha" type="password" placeholder="Digite sua senha" onChange={(e)=>setPassword(e.target.value)}/>
              <Input required legend="Confirme a Senha" type="password" placeholder="Digite sua senha" onChange={(e)=>setPasswordConfirm(e.target.value)}/>
             <Button type="submit" isLoading={isLoading}>Cadastrar</Button>
